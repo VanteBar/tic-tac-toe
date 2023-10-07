ac4b81483154fb897ebc98714d38a5b0fe1f9d7e
VanteBar <valeryanka.04@mail.ru>
2023-10-07 10:50:22 +0300

    Проверка победителя

--- public_html/js/js.js
+++ public_html/js/js.js
@@ -1,3 +1,50 @@
+let winner='';
+
+const contentWrapper=document.getElementById('content');
+const modalRes=document.getElementById('modal-res-wrapper');
+const overlay=document.getElementById('overlay');
+const btnClose=document.getElementById('btn-close');
+
+const checkWinner = () => {
+    const square = document.getElementsByClassName('square');
+    console.log(square);
+    const arr = [
+        [0, 1, 2],
+        [3, 4, 5], 
+        [6, 7, 8],
+        [0, 3, 6], 
+        [1, 4, 7], 
+        [2, 5, 8],
+        [0, 4, 8], 
+        [2, 4, 6]
+    ];
+    for(i =0;i<arr.length;i++){
+        if(square[arr[i][0]].innerHTML ==='X' && square[arr[i][1]].innerHTML ==='X' 
+                && square[arr[i][2]].innerHTML ==='X'){
+            winner='крестики';
+            prepare(winner);
+        } else if(square[arr[i][0]].innerHTML ==='O' && square[arr[i][1]].innerHTML ==='O' 
+                && square[arr[i][2]].innerHTML ==='O'){
+            winner='нолики';
+            prepare(winner);
+        }
+    }
+};
+
+const prepare = res =>{
+    contentWrapper.innerHTML=`Победили ${res} !`;
+    modalRes.style.display = 'block';
+};
+
+
+
+const closeModal =()=>{
+    modalRes.style.display='none';
+    location.reload();
+};
+
+overlay.addEventListener('click', closeModal);
+btnClose.addEventListener('click', closeModal);
 
 const btnO1 = document.getElementById('btnO1');
 const btnX1 = document.getElementById('btnX1');
@@ -26,8 +73,26 @@ function btnFuncO2(){
 
 
 const area = document.getElementById('area');
-const square = document.getElementsByClassName('square');
+
 let move=0;
+if ((btnX1.disabled===false) || (btnO1.disabled===false)){
+    area.addEventListener('click', e => {
+        if (e.target.className === 'square'){
+        move % 2 === 0 ? e.target.innerHTML = 'X' : e.target.innerHTML = 'O';
+        move++; 
+    checkWinner()} 
+    
+    }); } else{
+    alert('Игроки не выбрали символы!');
+}
+
+
+/*area.addEventListener('click', e => {
+        if (e.target.className === 'square'){
+        move % 2 === 0 ? e.target.innerHTML = 'X' : e.target.innerHTML = 'O';
+        move++;
+    } 
+});*/
 
 //function areaFunc() {
 //    if (area.className === 'square'){
@@ -37,22 +102,6 @@ let move=0;
 //};
 
 
-//document.getElementById('myAnchor').innerHTML="Новая ссылка";
-
-if ((btnX1.disabled===false) || (btnO1.disabled===false)){
-    area.addEventListener('click', e => {
-        if (e.target.className === 'square'){
-        move % 2 === 0 ? e.target.innerHTML = 'X' : e.target.innerHTML = 'O';
-        move++; } 
-    }); } else{
-    alert('Игроки не выбрали символы!');
-}
-//area.addEventListener('click', e => {
-//        if (e.target.className === 'square'){
-//        move % 2 === 0 ? e.target.innerHTML = 'X' : e.target.innerHTML = 'O';
-//        move++;
-//    } 
-//});
 
 //function areaFunc(){
 //    if (e.target.className === 'square'){
@@ -64,3 +113,4 @@ if ((btnX1.disabled===false) || (btnO1.disabled===false)){
        // alert('Игроки не выбрали символы!');}
        
 //(btnX1.disabled===true) || (btnO1.disabled===true))
+
